50abd02eda73b1a22e261dd6e9160d4145b6f4b3
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-04-16 15:40:57 -0600

    changes

--- codethecount.py
+++ codethecount.py
@@ -8,3 +8,21 @@ def running_count_value(card):
     elif card in ['10', 'J', 'Q', 'K', 'A']:
         return -1
     return 0
+
+def card_value(card):
+    if card in ['J', 'Q', 'K']:
+        return 10
+    elif card == 'A':
+        return 11
+    else:
+        return int(card)
+
+def card_counting():
+    print("\nStarting Black Jack Card Counting\n")
+
+    decks = int(input("Enter the number of decks in play: "))
+    cards_seen = 0
+    total_cards = decks * 52
+    running_count = 0
+    all_cards_dealt = []
+
